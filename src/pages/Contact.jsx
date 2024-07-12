32d2c919100f585b4ebb1426731862e3610a0a76
Lev <leva2polunin@gmail.com>
2024-07-12 12:22:16 +0800

    update contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,20 +2,20 @@ const Contact = () => {
     return ( 
         <main className="section">
         <div className="container">
-                <h1 className="title-1">Contacts</h1>
+                <h1 className="title-1">Как связаться со мной</h1>
 
                 <ul className="content-list">
                     <li className="content-list__item">
-                        <h2 className="title-2">Location</h2>
-                        <p>Moscow, Russia</p>
+                        <h2 className="title-2">Мой часовой пояс</h2>
+                        <p>Иркутск (+5 МСК), Россия</p>
                     </li>
                     <li className="content-list__item">
                         <h2 className="title-2">Telegram / WhatsApp</h2>
-                        <p><a href="tel:+79051234567">+7 (905) 123-45-67</a></p>
+                        <p><a href="tel:+79086617996">+7 (908) 661-79-96</a></p>
                     </li>
                     <li className="content-list__item">
                         <h2 className="title-2">Email</h2>
-                        <p><a href="mailto:webdev@protonmail.com">webdev@protonmail.com</a></p>
+                        <p><a href="mailto:webfrom38@gmail.com">webfrom38@gmail.com</a></p>
                     </li>
                 </ul>
 
